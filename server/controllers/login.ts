303b4799fcb7c3a4cee1f0b0ae7b6fe35cf2f063
cookie-for-pres <cookiedid911@protonmail.com>
2021-12-27 13:43:13 -0800

    fix

--- server/controllers/login.ts
+++ server/controllers/login.ts
@@ -1,3 +1,22 @@
+import Account from '../models/Account';
+
 export  default async (req: any, res: any) => {
-  
+  const { username, password } = req.body;
+  const account = await Account.findOne({ username });
+
+  if (username && password) {
+    if (account) {
+
+    } else {
+      res.status(404).json({
+        message: 'cant find account',
+        success: false
+      });
+    }
+  } else {
+    res.status(409).json({
+      message: 'username and password are required fields',
+      success: false
+    });
+  }
 }
