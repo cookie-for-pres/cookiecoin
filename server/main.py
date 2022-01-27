2fc617ba799beb9b3291b0dd1bcf713d077d2068
cookie-for-pres <cookiedid911@protonmail.com>
2022-01-26 20:06:34 -0800

    cors fix 2

--- server/main.py
+++ server/main.py
@@ -34,7 +34,10 @@ origins_file.close()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins
+    allow_origins=['*'],
+    allow_credentials=True,
+    allow_methods=['*'],
+    allow_headers=['*'],
 )
 
 app.include_router(login_router)
