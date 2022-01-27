11deaab578d7068f89b8f37c4ece794755d62f3f
cookie-for-pres <cookiedid911@protonmail.com>
2022-01-26 19:25:20 -0800

    fix

--- server-old/routes/leaderboard.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-
-import leaderboard from '../controllers/leaderboard';
-
-const router = Router();
-
-router.post('/leaderboard', leaderboard);
-
-export default router;
--- server/routes/dashboard.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-
-import dashboard from '../controllers/dashboard';
-
-const router = Router();
-
-router.post('/dashboard', dashboard);
-
-export default router;
